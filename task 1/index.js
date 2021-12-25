d376c10aecfc54d49a2b7b7aaed5ea59bdbc4ffe
Gokulpradap <94739280+Gokulpradap@users.noreply.github.com>
2021-12-25 20:16:29 +0530

    1st task

---
+++ task 1/index.js
@@ -0,0 +1,36 @@
+//string
+var Name = "guvi";
+console.log(typeof Name);
+
+
+// true (or) false
+let a = 10;
+let b = 10;
+let c = 20;
+console.log(a == b);
+console.log(a > c);
+
+//undefined (or) not defined
+let x;
+console.log(x);
+//console.log(y); //error
+
+// Array and object
+let k = [10, 20, 30];
+console.log(typeof k);
+
+let o = {};
+console.log(typeof o);
+
+
+// NaN
+const input = 'X100';
+const num = parseInt(input);
+
+console.log(num); 
+
+// null
+
+console.log(null == undefined);
+
+console.log(null === undefined);
